c6980f0723d1e863c5944e3284c0d87f6cdc7ace
Keyaksen <pur.oks.pur@gmail.com>
2018-11-03 22:58:57 +0700

    Добавлены 3 метода sqrt, pow, cos

---
+++ src/main/java/ru/fintech/TestClass.java
@@ -0,0 +1,16 @@
+package ru.fintech;
+
+public abstract class TestClass {
+
+    public static double testSqrt(double num) {
+        return Math.sqrt(num);
+    }
+
+    public static double testPow2(double num, double pow1) {
+        return Math.pow(num,2.0);
+    }
+
+    public static double testCos(double num) {
+        return Math.cos(num);
+    }
+}
---
+++ src/test/java/ru/fintech/TestExample.java
@@ -0,0 +1,31 @@
+package ru.fintech;
+
+import org.junit.Test;
+
+
+import static org.junit.Assert.assertEquals;
+import static ru.fintech.TestClass.testCos;
+import static ru.fintech.TestClass.testPow2;
+import static ru.fintech.TestClass.testSqrt;
+public class TestExample {
+    @Test
+    public void testExampleSqrt(){
+        double result = testSqrt(6.66);
+        assertEquals("Wrong sqty!", result, 2.5806975801127883, 0);
+    }
+
+    @Test
+    public void testExamplePow2(){
+        double result = testPow2(6.66, 2.0);
+        assertEquals("Wrong pow!", result, 44.3556, 0);
+    }
+
+    @Test
+    public void testExampleCos(){
+        double result = testCos(6.66);
+        assertEquals("Wrong cos!", result, 0.9298414180167014, 0);
+    }
+
+
+
+}
