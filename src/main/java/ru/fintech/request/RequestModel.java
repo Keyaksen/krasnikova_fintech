962cd1e5ebb6c311dfce84ada63e56ae8c867d94
Keyaksen <pur.oks.pur@gmail.com>
2018-11-18 23:41:15 +0700

    Изменения в RequestModel, в json-файле, созданы тесты по 3 запросам

--- src/main/java/ru/fintech/request/RequestModel.java
+++ src/main/java/ru/fintech/request/RequestModel.java
@@ -18,6 +18,9 @@ public abstract class RequestModel {
                 .setBaseUri(BASE_URI)
                 .setBasePath(BASE_PATH)
                 .setAuth(authScheme)
+                .addQueryParam("request_id", "84g5df1g-5fg6-7d5f-1e61-874d54tfb15")
+                .addQueryParam("system_code", "T-API")
+                .addPathParam("siebel_id", "k.krasnikova")
                 .addFilter(new RequestLoggingFilter())
                 .addFilter(new ResponseLoggingFilter())
                 //.addFilter(new ErrorLoggingFilter())  //for error demonstrate
@@ -27,6 +30,27 @@ public abstract class RequestModel {
     public static ResponseSpecification getResponseSpecification() {
         return new ResponseSpecBuilder()
                 .expectContentType(ContentType.JSON)
+                .build();
+    }
+    public static RequestSpecification getRequestSpecification1() { //for error test getSubscriptionsWithoutParamsTest
+        PreemptiveBasicAuthScheme authScheme = new PreemptiveBasicAuthScheme();
+        authScheme.setUserName(USER_LOGIN);
+        authScheme.setPassword(USER_PASS);
+        return new RequestSpecBuilder()
+                .setBaseUri(BASE_URI)
+                .setBasePath(BASE_PATH)
+                .setAuth(authScheme)
+                .addPathParam("siebel_id", "k.krasnikova")
+                .addFilter(new RequestLoggingFilter())
+                .addFilter(new ResponseLoggingFilter())
+                //.addFilter(new ErrorLoggingFilter())  //for error demonstrate
+                .build();
+    }
+
+    public static ResponseSpecification getResponseSpecification1() {//for error test getSubscriptionsWithoutParamsTest
+        return new ResponseSpecBuilder()
+                .expectContentType(ContentType.JSON)
+                .expectStatusCode(400)
                 .build();
     }
 
--- src/test/java/ru/fintech/lesson7/SubscriptionsOptimizeTest.java
+++ src/test/java/ru/fintech/lesson7/SubscriptionsOptimizeTest.java
@@ -1,26 +1,31 @@
 package ru.fintech.lesson7;
 
 import io.restassured.module.jsv.JsonSchemaValidator;
+import org.hamcrest.Matchers;
 import org.json.JSONObject;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import ru.fintech.entity.SubInfo;
 import ru.fintech.request.RequestModel;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import static io.restassured.RestAssured.given;
 
 public class SubscriptionsOptimizeTest {
 public String idCode;
+
+
+
     @Test
+    @DisplayName("Get all subscriptions")
     @Tag("get")
     public void getSubscriptionsSimpleTest(){
+        System.out.println("-----------\"GET\" test-----------");
         given().spec(RequestModel.getRequestSpecification())
-                .queryParam("request_id", "6f994192-e701-11e8-9f32-f2801f1b9fd1")
-                .queryParam("system_code", "T-API")
-                .get("/contacts/k.krasnikova/subscriptions")
+                .get("/contacts/{siebel_id}/subscriptions")
                 .then()
                 .specification(RequestModel.getResponseSpecification())
                 .assertThat()
@@ -28,95 +33,128 @@ public String idCode;
     }
 
    @Test
+   @DisplayName("Get all subscriptions to List")
    @Tag("get")
    public void getSubscriptionsExtractTest(){
-        List<SubInfo>  subInfo = new ArrayList<SubInfo>() ;
-                subInfo.add(given().spec(RequestModel.getRequestSpecification())
-                .queryParam("request_id", "6f994192-e701-11e8-9f32-f2801f1b9fd1")
-                .queryParam("system_code", "T-API")
-                .get("/contacts/k.krasnikova/subscriptions")
+        System.out.println("-----------\"GET\" test-----------");
+        List<SubInfo> subInfo = given().spec(RequestModel.getRequestSpecification())
+                .get("/contacts/{siebel_id}/subscriptions")
                 .then()
                 .specification(RequestModel.getResponseSpecification())
                 .assertThat()
                 .statusCode(200)
                 .extract()
-                .as(SubInfo.class));
+                .body()
+                .jsonPath()
+                .getList(".",SubInfo.class);
         System.out.println("Result: " + subInfo);
     }
 
-   /* @Test
-    public void getSubscriptionsErrorTest(){
-        given().spec(RequestModel.getRequestSpecification())
-                .queryParam("request_id", "6f994192-e701-11e8-9f32-f2801f1b9fd1")
-                .queryParam("system_code", "T-API")
-                .get("/contacts/k.krasnikova/subscriptions")
+    @Test
+    @DisplayName("Get all subscriptions without parameters")
+    @Tag("get")
+    public void getSubscriptionsWithoutParamsTest(){
+        System.out.println("-----------\"GET\" test-----------");
+        given().spec(RequestModel.getRequestSpecification1())
+                //.queryParam("request_id", "6f994192-e701-11e8-9f32-f2801f1b9fd1")
+                //.queryParam("system_code", "T-API")
+                .pathParam("siebel_id", "k.krasnikova")
+                .get("/contacts/{siebel_id}/subscriptions")
                 .then()
-                .specification(RequestModel.getResponseSpecification())
-                .statusCode(404)
-                ;
-    }*/
+                .specification(RequestModel.getResponseSpecification1())
+                .assertThat()
+                .body("error", Matchers.equalTo("missing \"request_id\" in query"));
+    }
 
+   //@BeforeEach
    @Test
+   @DisplayName("Get all id of subscriptions")
    @Tag("get")
-    public void getInstrumentsExtractIdCodeTest(){
+    public void getSubscriptionsIdCodeTest(){
+        System.out.println("-----------\"GET\" test-----------");
         idCode = given().spec(RequestModel.getRequestSpecification())
-                .queryParam("request_id", "6f994192-e701-11e8-9f32-f2801f1b9fd1")
-                .queryParam("system_code", "T-API")
-                .get("/contacts/k.krasnikova/subscriptions")
+                .get("/contacts/{siebel_id}/subscriptions")
                 .then()
                 .specification(RequestModel.getResponseSpecification())
                 .assertThat()
                 .statusCode(200)
-                //.body(JsonSchemaValidator.matchesJsonSchemaInClasspath("subs_info.json"))
+                .body(JsonSchemaValidator.matchesJsonSchemaInClasspath("subs_info.json"))
                 .extract()
                 .jsonPath()
                 .getString("id");
         System.out.println("Result: " + idCode);
     }
-    @Test
-    @Tag("post")
-    public void postSubscriptionsSimpleTest(){
+
+    //creating JSON object for POST query
+    public static JSONObject createJSONObject(){
         JSONObject requestParams = new JSONObject();
-        requestParams.put("instrument_id", "GAZP_TQBR");
-        requestParams.put("sec_name", "GAZP");
+        requestParams.put("instrument_id", "TCS_SPBXM");
+        requestParams.put("sec_name", "TCS");
         requestParams.put("sec_type","equity");
         requestParams.put("price_alert",15);
+        return requestParams;
+    }
+
+    @Test
+    @DisplayName("Creating price-alert subscription")
+    @Tag("post")
+    public void postSubscriptionsTest(){
+        System.out.println("-----------\"POST\" test-----------");
         given().spec(RequestModel.getRequestSpecification())
-                .queryParam("request_id", "6f994192-e701-11e8-9f32-f2801f1b9fd1")
-                .queryParam("system_code", "T-API")
-                .body(requestParams)
-                .post("/contacts/k.krasnikova/subscriptions")
+                .body(createJSONObject())
+                .post("/contacts/{siebel_id}/subscriptions")
                 .then()
                 .specification(RequestModel.getResponseSpecification())
                 .assertThat()
                 .statusCode(200);
     }
+
     @Test
-    @Tag("delete")
-    public void deleteSubscriptionsSimpleTest(){
-        idCode = given().spec(RequestModel.getRequestSpecification())
-                .queryParam("request_id", "6f994192-e701-11e8-9f32-f2801f1b9fd1")
-                .queryParam("system_code", "T-API")
-                .get("/contacts/k.krasnikova/subscriptions")
+    @DisplayName("Creating duplicate subscription")
+    @Tag("post")
+    public void postDuplicateSubscriptionsTest(){
+        System.out.println("-----------\"POST\" test-----------");
+        given().spec(RequestModel.getRequestSpecification())
+                .body(createJSONObject())
+                .post("/contacts/{siebel_id}/subscriptions")
                 .then()
                 .specification(RequestModel.getResponseSpecification())
                 .assertThat()
-                .statusCode(200)
-                //.body(JsonSchemaValidator.matchesJsonSchemaInClasspath("instrument_info.json"))
-                .extract()
-                .jsonPath()
-                .getString("id");
+                .statusCode(409)
+                .body("error", Matchers.equalTo("could not create subscription: subscription constraint violated"));
+   }
+
+    @Test
+    @DisplayName("Deleting first subscription")
+    @Tag("delete")
+    public void deleteSubscriptionsTest(){
+        System.out.println("recall GET query");
+        getSubscriptionsIdCodeTest();
+        System.out.println("-----------\"DELETE\" test-----------");
         idCode=idCode.substring(1);
+        idCode=idCode.substring(0,idCode.length()-1);
         String []splitArray = idCode.split(",");
         given().spec(RequestModel.getRequestSpecification())
                 .pathParam("subscription_id",splitArray[0])
-                .queryParam("request_id", "6f994192-e701-11e8-9f32-f2801f1b9fd1")
-                .queryParam("system_code", "T-API")
-                .delete("/contacts/k.krasnikova/subscriptions/{subscription_id}")
+                .delete("/contacts/{siebel_id}/subscriptions/{subscription_id}")
                 .then()
-                //.specification(RequestModel.getResponseSpecification())
                 .assertThat()
                 .statusCode(200);
+    }
+
+    @Test
+    @DisplayName("Deleting nonexistent subscription")
+    @Tag("delete")
+    public void deleteSubscriptionsErrorTest(){
+        System.out.println("-----------\"DELETE\" test-----------");
+        idCode="123abc123";
+        given().spec(RequestModel.getRequestSpecification())
+                .pathParam("subscription_id",idCode)
+                .delete("/contacts/{siebel_id}/subscriptions/{subscription_id}")
+                .then()
+                .assertThat()
+                .statusCode(500)
+                .body("error", Matchers.equalTo("could not cancel subscription: pq: invalid input syntax for uuid: \"" + idCode+"\""));
     }
 
 }
